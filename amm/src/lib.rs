bed91a219844f459d2de29b24fe1bad4065ecd51
cutelittlebeau <beauji116jbn@gmail.com>
2022-07-22 07:17:36 +0800

    feat: finished core logic again

--- amm/src/lib.rs
+++ amm/src/lib.rs
@@ -1,24 +1,17 @@
-use std::ops::{Add, Div, Mul};
-
 use near_contract_standards::fungible_token::core::FungibleTokenCore;
 use near_contract_standards::fungible_token::metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider};
-
 use near_contract_standards::fungible_token::resolver::FungibleTokenResolver;
-use near_sdk::{AccountId, assert_one_yocto, assert_self, Balance, env, ext_contract, Gas, is_promise_success, log, near_bindgen, Promise, PromiseOrValue, PromiseResult};
+use near_contract_standards::storage_management::StorageBalance;
+use near_sdk::{AccountId, Balance, env, ext_contract, Gas, log, near_bindgen, Promise, PromiseOrValue, PromiseResult, serde_json};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
-use rust_decimal::Decimal;
-use rust_decimal::prelude::ToPrimitive;
-
-pub const CALLBACK_GAS: Gas = Gas(5_000_000_000_000);
-
-const INITIAL_BALANCE: Balance = 100_000_000_000_000_000_000_000_000; // 2.5e23yN, 0.25N
 
 const CODE: &[u8] = include_bytes!("../../amm_wallet/res/amm_wallet.wasm");
+const N: Balance = 1_000_000_000_000_000_000_000_000;
 
 #[ext_contract(ext_ft)]
-pub trait FungibleToken<T = Self>
+pub trait FungibleTokenContract<T = Self>
     where
         T: FungibleTokenCore
         + FungibleTokenMetadataProvider
@@ -41,51 +34,31 @@ pub trait FungibleToken<T = Self>
         receiver_id: AccountId,
         amount: U128,
     ) -> U128;
+    fn storage_deposit(
+        &mut self,
+        account_id: Option<AccountId>,
+        registration_only: Option<bool>,
+    ) -> StorageBalance;
 }
 
-#[ext_contract(ext_sub)]
-pub trait SubContract {
-    fn init(a: AccountId, b: AccountId) -> Self;
-
-    fn withdraw_a(&mut self, account_id: AccountId, qty: Balance) -> Promise;
-
-    fn withdraw_b(&mut self, account_id: AccountId, qty: Balance) -> Promise;
+#[ext_contract(ext_wallet)]
+pub trait AmmWalletContract {
+    fn init(a: AccountId, a_meta: FungibleTokenMetadata, b: AccountId, b_meta: FungibleTokenMetadata) -> Self;
 }
 
 #[near_bindgen]
 #[derive(Clone, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Contract {
-    pub a: Token,
-    pub b: Token,
-    pub wallet: AccountId,
+    wallet: AccountId,
     initialized: bool,
-}
-
-#[near_bindgen]
-#[derive(Clone, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-#[serde(crate = "near_sdk::serde")]
-pub struct Token {
-    addr: AccountId,
-    meta: Option<FungibleTokenMetadata>,
-    qty: Balance,
 }
 
 // Define the default, which automatically initializes the contract
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            a: Token {
-                addr: env::current_account_id(),
-                meta: None,
-                qty: Default::default(),
-            },
-            b: Token {
-                addr: env::current_account_id(),
-                meta: None,
-                qty: Default::default(),
-            },
-            wallet: env::current_account_id(),
+            wallet: AccountId::new_unchecked("a".repeat(64)),
             initialized: false,
         }
     }
@@ -93,236 +66,135 @@ impl Default for Contract {
 
 #[near_bindgen]
 impl Contract {
-    fn assert_initialized(&mut self) {
-        assert!(self.initialized, "contract is not initialized yet")
-    }
-    fn assert_deposited(&mut self){
-        assert!(self.a.qty * self.b.qty > 0, "must deposit for both tokens before swap")
-    }
-    pub fn init(&mut self, a: String, b: String) -> Promise {
+    pub fn init(&mut self, a: AccountId, b: AccountId) -> Promise {
+        assert!(!self.initialized, "contract is already initialized");
+
         log!("start init, a: {}, b: {}", a, b);
-        let a_account_id = AccountId::try_from(a).unwrap();
-        let b_account_id = AccountId::try_from(b).unwrap();
-
-        self.a.addr = a_account_id.clone();
-        self.b.addr = b_account_id.clone();
-        let p_set_a_meta = self.solve_token_metadata(a_account_id.clone());
-        let p_set_b_meta = self.solve_token_metadata(b_account_id.clone());
-        let p_create_wallet = self.create_wallet(a_account_id, b_account_id);
-        p_set_a_meta.and(p_set_b_meta).and(p_create_wallet).then(
-            Self::ext(env::current_account_id())
-                .with_unused_gas_weight(1)
-                .init_done(),
-        )
+        let p_fetch_meta_a = ext_ft::ext(a.clone()).ft_metadata();
+        let p_fetch_meta_b = ext_ft::ext(b.clone()).ft_metadata();
+        let p_create_wallet = Self::ext(env::current_account_id())
+            .with_static_gas(Gas(200_000_000_000_000))
+            .create_wallet_with_metadata(a, b);
+        let p_init_done = Self::ext(env::current_account_id())
+            .with_unused_gas_weight(1)
+            .init_done();
+        p_fetch_meta_a.and(p_fetch_meta_b)
+            .then(p_create_wallet)
+            .then(p_init_done)
     }
 
     #[private]
-    pub fn solve_token_metadata(&mut self, addr: AccountId) -> Promise {
-        ext_ft::ext(addr.clone()).ft_metadata().then(
-            Self::ext(env::current_account_id())
-                // .with_static_gas(CALLBACK_GAS)
-                .with_unused_gas_weight(1)
-                .solve_token_metadata_callback(addr),
-        )
-    }
-    #[private]
-    pub fn solve_token_metadata_callback(&mut self, addr: AccountId) {
-        // handle the result from the cross contract call this method is a callback for
-        let meta: FungibleTokenMetadata = match env::promise_result(0) {
+    pub fn create_wallet_with_metadata(&mut self, a: AccountId, b: AccountId) -> Promise {
+        assert_eq!(env::promise_results_count(), 2, "should have 2 metadata results");
+        let md_a: FungibleTokenMetadata = match env::promise_result(0) {
             PromiseResult::NotReady => unreachable!(),
             PromiseResult::Failed => panic!("Failed to get token metadata"),
             PromiseResult::Successful(result) => {
-                near_sdk::serde_json::from_slice::<FungibleTokenMetadata>(&result)
+                serde_json::from_slice::<FungibleTokenMetadata>(&result)
                     .unwrap()
             }
         };
-        log!(
-            "got metadata of {}: {}",
-            addr,
-            near_sdk::serde_json::to_string(&meta).unwrap()
-        );
-        if addr == self.a.addr {
-            self.a.meta = Option::from(meta)
-        } else if addr == self.b.addr {
-            self.b.meta = Option::from(meta)
-        } else {
-            panic!("Unexpected address {}", addr)
-        }
+        let md_b: FungibleTokenMetadata = match env::promise_result(1) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Failed => panic!("Failed to get token metadata"),
+            PromiseResult::Successful(result) => {
+                serde_json::from_slice::<FungibleTokenMetadata>(&result)
+                    .unwrap()
+            }
+        };
+        log!("a: {}, md a: {}", a, serde_json::to_string(&md_a).unwrap());
+        log!("b: {}, md b: {}",b,serde_json::to_string(&md_b).unwrap());
+        self.init_wallet(a, md_a, b, md_b)
     }
-    #[private]
-    pub fn create_wallet(&mut self, a: AccountId, b: AccountId) -> Promise {
+
+    fn init_wallet(&mut self, a: AccountId, a_metadata: FungibleTokenMetadata, b: AccountId, b_metadata: FungibleTokenMetadata) -> Promise {
         let wallet_account_id =
             AccountId::new_unchecked(format!("{}_{}.{}", "wallet", env::block_height() % 10000, env::current_account_id()));
+        log!("wallet account id: {}", wallet_account_id);
 
-        let p_create_account = Promise::new(wallet_account_id.clone())
+        let p_deploy_wallet_contract = Promise::new(wallet_account_id.clone())
             .create_account()
             .add_full_access_key(env::signer_account_pk())
-            .transfer(INITIAL_BALANCE)
+            .transfer(30 * N)
             .deploy_contract(CODE.to_vec());
-        let p_deploy_code = ext_sub::ext(wallet_account_id.clone())
-            .init(a, b);
-        p_create_account.then(p_deploy_code).then(
-            Self::ext(env::current_account_id())
-                .with_unused_gas_weight(1)
-                .create_wallet_callback(wallet_account_id),
-        )
+        let p_init_wallet_contract = ext_wallet::ext(wallet_account_id.clone())
+            .with_unused_gas_weight(1)
+            .init(a.clone(), a_metadata, b.clone(), b_metadata);
+        let p_register = self.register_account(wallet_account_id.clone(), a, b);
+        let p_callback = Self::ext(env::current_account_id())
+            .with_unused_gas_weight(1)
+            .create_wallet_callback(wallet_account_id);
+
+        p_deploy_wallet_contract
+            .then(p_init_wallet_contract)
+            .and(p_register)
+            .then(p_callback)
     }
+
+    fn register_account(&mut self, wallet_account_id: AccountId, a: AccountId, b: AccountId) -> Promise {
+        let p_register_self_to_a = ext_ft::ext(a.clone())
+            .with_attached_deposit(N)
+            .storage_deposit(
+                None,
+                None,
+            );
+        let p_register_self_to_b = ext_ft::ext(b.clone())
+            .with_attached_deposit(N)
+            .storage_deposit(
+                None,
+                None,
+            );
+        let p_register_wallet_to_a = ext_ft::ext(a)
+            .with_attached_deposit(N)
+            .storage_deposit(
+                Some(wallet_account_id.clone()),
+                None,
+            );
+        let p_register_wallet_to_b = ext_ft::ext(b)
+            .with_attached_deposit(N)
+            .storage_deposit(
+                Some(wallet_account_id),
+                None,
+            );
+        p_register_self_to_a
+            .and(p_register_self_to_b)
+            .and(p_register_wallet_to_a)
+            .and(p_register_wallet_to_b)
+    }
+
     #[private]
     pub fn create_wallet_callback(&mut self, wallet_account_id: AccountId) {
+        log!("promise result count in create_wallet_callback: {}", env::promise_results_count());
+        assert_all_result_success();
         log!("create wallet {} done", wallet_account_id);
         self.wallet = wallet_account_id
     }
+
     #[private]
     pub fn init_done(&mut self) {
+        log!("promise result count in init_done: {}", env::promise_results_count());
+        assert_all_result_success();
         log!("init done");
         self.initialized = true
     }
 
-    pub fn stat(&mut self) -> Contract {
+    pub fn state(&mut self) -> Contract {
         self.clone()
     }
 
-    #[payable]
-    pub fn deposit_a(&mut self, qty: U128) -> Promise {
-        self.assert_initialized();
-        // 1. assert self
-        assert_self();
-        assert_one_yocto();
-        // 2. validate input
-        assert!(qty > U128(0), "At least one quantity of a and b must be positive");
-        ext_ft::ext(self.a.addr.clone())
-            .with_attached_deposit(1)
-            .ft_transfer(
-                self.wallet.clone(),
-                qty,
-                Some("deposit from amm contract".to_string()),
-            )
-            .then(Self::ext(env::current_account_id()).deposit_a_callback(qty))
-    }
-
-    #[private]
-    pub fn deposit_a_callback(&mut self, qty: U128) {
-        assert!(is_promise_success(), "failed to transfer");
-        self.a.qty += Balance::from(qty)
-    }
-
-    #[payable]
-    pub fn deposit_b(&mut self, qty: U128) -> Promise {
-        self.assert_initialized();
-        // 1. assert self
-        assert_self();
-        assert_one_yocto();
-        // 2. validate input
-        assert!(qty > U128(0), "At least one quantity of a and b must be positive");
-        ext_ft::ext(self.b.addr.clone())
-            .with_attached_deposit(1)
-            .ft_transfer(
-                self.wallet.clone(),
-                qty,
-                Some("deposit from amm contract".to_string()),
-            )
-            .then(
-                Self::ext(env::current_account_id())
-                    .deposit_b_callback(qty))
-    }
-
-    #[private]
-    pub fn deposit_b_callback(&mut self, qty: U128) {
-        assert!(is_promise_success(), "failed to transfer");
-        self.b.qty += Balance::from(qty)
-    }
-
-    #[payable]
-    pub fn swap_a(&mut self, qty: U128) -> Promise {
-        self.assert_initialized();
-        self.assert_deposited();
-        assert!(qty > U128(0), "input quantity must be positive");
-
-        let b_diff = get_des_diff_from_src_qty(self.a.qty, qty.into(), self.b.qty);
-        log!("current a qty: {}, b qty: {}, get b diff in swap a: {}",self.a.qty, self.b.qty, b_diff);
-
-        // try to send qty a from user to wallet
-        let p_send_a = ext_ft::ext(self.a.addr.clone())
-            .with_attached_deposit(1)
-            .with_unused_gas_weight(1)
-            .ft_resolve_transfer(
-                env::predecessor_account_id(),
-                self.wallet.clone(),
-                qty,
-            );
-        // try to send b_diff b from wallet to user
-        let p_receive_b = ext_sub::ext(self.wallet.clone())
-            .with_attached_deposit(1)
-            .with_unused_gas_weight(1)
-            .withdraw_b(
-                env::signer_account_id(),
-                b_diff,
-            );
-        // try to update balance
-        p_send_a
-            .then(p_receive_b)
-            .then(
-                Self::ext(env::current_account_id())
-                    .with_unused_gas_weight(1)
-                    .swap_a_callback(qty, b_diff.into()))
-    }
-
-    #[private]
-    pub fn swap_a_callback(&mut self, qty: U128, diff: U128) {
-        log!("swap callback promise result count: {}", env::promise_results_count() );
-        self.a.qty += Balance::from(qty);
-        self.b.qty -= Balance::from(diff);
-    }
-
-    #[payable]
-    pub fn swap_b(&mut self, qty: U128) -> Promise {
-        self.assert_initialized();
-        self.assert_deposited();
-        assert!(qty > U128(0), "input quantity must be positive");
-
-        let a_diff = get_des_diff_from_src_qty(self.b.qty, qty.into(), self.a.qty);
-        log!("current a qty: {}, b qty: {}, get a diff in swap b: {}",self.a.qty, self.b.qty, a_diff);
-        // try to send qty a from user to wallet
-        let p_send_b = ext_ft::ext(self.b.addr.clone())
-            .with_attached_deposit(1)
-            .with_unused_gas_weight(1)
-            .ft_resolve_transfer(
-                env::predecessor_account_id(),
-                self.wallet.clone(),
-                qty,
-            );
-        // try to send b_diff b from wallet to user
-        let p_receive_a = ext_sub::ext(self.wallet.clone())
-            .with_attached_deposit(1)
-            .with_unused_gas_weight(1)
-            .withdraw_a(
-                env::signer_account_id(),
-                a_diff,
-            );
-        p_send_b
-            .then(p_receive_a)
-            .then(
-                Self::ext(env::current_account_id())
-                    .with_unused_gas_weight(1)
-                    .swap_b_callback(qty, a_diff.into()))
-    }
-
-    #[private]
-    pub fn swap_b_callback(&mut self, qty: U128, diff: U128) {
-        log!("swap callback promise result count: {}", env::promise_results_count() );
-        self.b.qty += Balance::from(qty);
-        self.a.qty -= Balance::from(diff);
+    pub fn update_wallet_contract(&mut self) -> Promise {
+        Promise::new(self.wallet.clone())
+            .add_full_access_key(env::signer_account_pk())
+            .deploy_contract(CODE.to_vec())
     }
 }
 
-// des_qty = des_balance - src_balance * des_balance / (src_balance + src_qty)
-//         = des_balance * src_qty / (src_balance + src_qty)
-fn get_des_diff_from_src_qty(src_balance: Balance, src_qty: Balance, des_balance: Balance) -> Balance {
-    let src_diff_dec = Decimal::from(src_qty);
-    let src_qty_dec = Decimal::from(src_balance);
-    let des_qty_dec = Decimal::from(des_balance);
-    des_qty_dec.mul(src_diff_dec)
-        .div(
-            src_qty_dec.add(src_diff_dec)
-        ).to_u128().unwrap()
+fn assert_all_result_success() {
+    for i in 0..env::promise_results_count() {
+        match env::promise_result(i) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Failed => panic!("promise result {} failed", i),
+            PromiseResult::Successful(_) => {}
+        };
+    }
 }
--- amm_wallet/src/lib.rs
+++ amm_wallet/src/lib.rs
@@ -1,60 +1,118 @@
-use near_sdk::{AccountId, Balance, env, ext_contract, Gas, near_bindgen, PanicOnDefault, Promise};
+use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
+use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
+use near_sdk::{AccountId, Balance, env, ext_contract, Gas, log, near_bindgen, PanicOnDefault, PromiseOrValue};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
 
 pub const CALLBACK_GAS: Gas = Gas(5_000_000_000_000);
 
 #[ext_contract(ext_ft)]
-pub trait FungibleToken {
-    fn caller_transfer(&mut self, account_id: AccountId, balance: Balance, memo: Option<String>);
+pub trait FungibleTokenContract {
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
 }
 
 #[near_bindgen]
 #[derive(Clone, Serialize, Deserialize, BorshDeserialize, BorshSerialize, PanicOnDefault)]
 #[serde(crate = "near_sdk::serde")]
-pub struct SubContract {
-    pub a: AccountId,
-    pub b: AccountId,
-    pub owner: AccountId,
+pub struct AmmWallet {
+    a: AccountId,
+    a_meta: FungibleTokenMetadata,
+    a_balance: Balance,
 
+    b: AccountId,
+    b_meta: FungibleTokenMetadata,
+    b_balance: Balance,
+
+    k: Balance,
+
+    owner: AccountId,
 }
 
 #[near_bindgen]
-impl SubContract {
+impl AmmWallet {
     #[init]
-    pub fn init(a: AccountId, b: AccountId) -> Self {
+    pub fn init(a: AccountId, a_meta: FungibleTokenMetadata, b: AccountId, b_meta: FungibleTokenMetadata) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
         Self {
             a,
+            a_meta,
+            a_balance: 0u128,
             b,
+            b_meta,
+            b_balance: 0u128,
+            k: 0u128,
             owner: env::predecessor_account_id(),
         }
     }
 
-    fn assert_owner(&mut self) {
-        assert_eq!(env::predecessor_account_id(), self.owner, "this contract can only be called by its owner")
+    pub fn state(&mut self) -> Self {
+        self.clone()
     }
 
-    #[payable]
-    pub fn withdraw_a(&mut self, account_id: AccountId, qty: Balance) -> Promise {
-        self.assert_owner();
-        ext_ft::ext(self.a.clone())
-            .with_attached_deposit(1)
-            .caller_transfer(
-                account_id,
-                qty,
-                Some("withdraw from amm sub contract".to_string()),
-            )
+    #[private]
+    pub fn on_transfer_a_back(&mut self, amount: U128) -> PromiseOrValue<U128> {
+        self.a_balance -= Balance::from(amount);
+        PromiseOrValue::Value(U128(0))
     }
+    #[private]
+    pub fn on_transfer_b_back(&mut self, amount: U128) -> PromiseOrValue<U128> {
+        self.b_balance -= Balance::from(amount);
+        PromiseOrValue::Value(U128(0))
+    }
+}
 
-    #[payable]
-    pub fn withdraw_b(&mut self, account_id: AccountId, qty: Balance) -> Promise {
-        self.assert_owner();
-        ext_ft::ext(self.b.clone())
-            .with_attached_deposit(1)
-            .caller_transfer(
-                account_id,
-                qty,
-                Some("withdraw from amm sub contract".to_string()),
-            )
+#[near_bindgen]
+impl FungibleTokenReceiver for AmmWallet {
+    fn ft_on_transfer(&mut self, sender_id: AccountId, amount: U128, msg: String) -> PromiseOrValue<U128> {
+        log!("ft_on_transfer called on AmmWallet, sender_id: {}, amount: {}, msg: {}", sender_id, amount.0, msg);
+        if env::predecessor_account_id() == self.a {
+            log!("called by a");
+            self.a_balance += Balance::from(amount);
+            if sender_id != self.owner {
+                log!("receive not from owner");
+                let b_diff = self.b_balance - self.k / (self.a_balance);
+                log!("b_diff: {}", b_diff);
+                if b_diff > 0 {
+                    return ext_ft::ext(self.b.clone())
+                        .with_attached_deposit(1)
+                        .ft_transfer(
+                            sender_id,
+                            b_diff.into(),
+                            Some("deposit b back to user".to_string()),
+                        ).then(
+                        Self::ext(env::current_account_id())
+                            .on_transfer_b_back(b_diff.into())).into();
+                }
+            } else {
+                log!("receive from owner, update k");
+                self.k = self.a_balance * self.b_balance;
+                log!("k is updated to {}", self.k)
+            }
+        } else if env::predecessor_account_id() == self.b {
+            log!("called by b");
+            self.b_balance += Balance::from(amount);
+            if sender_id != self.owner {
+                log!("receive not from owner");
+                let a_diff = self.a_balance - self.k / (self.b_balance);
+                log!("a_diff: {}", a_diff);
+                if a_diff > 0 {
+                    return ext_ft::ext(self.a.clone())
+                        .with_attached_deposit(1)
+                        .ft_transfer(
+                            sender_id,
+                            a_diff.into(),
+                            Some("deposit a back to user".to_string()),
+                        ).then(
+                        Self::ext(env::current_account_id())
+                            .on_transfer_a_back(a_diff.into())).into();
+                }
+            } else {
+                log!("receive from owner, update k");
+                self.k = self.a_balance * self.b_balance;
+                log!("k is updated to {}", self.k)
+            }
+        }
+        PromiseOrValue::Value(U128(0))
     }
 }
